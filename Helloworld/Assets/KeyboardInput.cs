498dc643fa316d4ed82b8e90a7ca14c886c46a63
Joseph Allison <allij300@coyote.csusb.edu>
2014-10-23 21:44:15 -0700

    new

---
+++ Helloworld/Assets/KeyboardInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardInput : MonoBehaviour {
+	public float moveSpeed =.05f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 tempPosition;
+		If (input.getaxis (“Horizontal”) > 0)){
+			tempPosition.x += moveSpeed;
+		}
+		If (input.getaxis (“Horizontal”) <0)){
+			tempPosition.x -= moveSpeed;
+		}
+		If (input.getaxis (“Vertical”) > 0)){
+			tempPosition.y += moveSpeed;
+		}
+		If (input.getaxis (“Vertical”) < 0)){
+			tempPosition.y -= moveSpeed;
+		}
+		Transform.position = tempPosition;
+
+	}
+}
---
+++ Helloworld/Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class NewBehaviourScript : MonoBehaviour {
+	public Sprite sprite1;
+	public Sprite sprite2;
+	// Use this for initializatio
+	void Start () {
+		SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)){
+				if (SpriteRenderer.sprite == sprite1) {
+						SpriteRenderer.sprite = sprite2;
+				}
+		else{
+			SpriteRenderer.sprite = sprite1;
+		}
+	}
+
+	}
+}
