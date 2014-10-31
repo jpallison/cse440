208d9012af53f0e9bc8d0af76fed6c8b09edbdcf
Joseph Allison <allij300@coyote.csusb.edu>
2014-10-30 17:03:06 -0700

    Gameworld tutorial

---
+++ gameworld/Assets/playermovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playermovement : MonoBehaviour {
+
+	public float moveSpeed;
+	private Vector3 moveDirection;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		// 2
+		if( Input.GetButton("Fire1") ) {
+			// 3
+			Vector3 moveToward = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+			// 4
+			moveDirection = moveToward - currentPosition;
+			moveDirection.z = 0; 
+			moveDirection.Normalize();
+		}
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+	}
+}
