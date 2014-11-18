a45bc4a13ecbe2c2ef16573b692b2a4e909cb0d4
Joseph Allison <allij300@coyote.csusb.edu>
2014-11-17 20:00:19 -0800

    new

---
+++ neighborhoodtroubles/Assets/Jump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+	public float jumpHeight;
+	public float multiplier;
+	public bool isJumping = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				
+				if (Input.GetKeyDown (KeyCode.Space)) {
+						if (!isJumping) {
+								rigidbody2D.AddForce (Vector2.up * jumpHeight);
+								isJumping = true;
+						}
+				}
+
+		}
+	void OnCollisonEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Ground") {
+			isJumping = false;		
+		}
+	}
+
+}
---
+++ neighborhoodtroubles/Assets/keyboardInput.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyboardInput : MonoBehaviour {
+	public float moveSpeed;
+	bool facingRight = true;
+	void Start ()
+	{
+	
+	}
+
+	void Update () {
+		movement ();
+	}
+	void movement()
+	{
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.Translate(new Vector3(-moveSpeed, 0, 0));
+			if (facingRight)
+			{
+				Flip ();
+			}
+			facingRight = false;
+		}
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.Translate(new Vector3(moveSpeed, 0, 0));
+			if(!facingRight)
+			{
+				Flip ();
+			}
+			facingRight = true;
+		}
+		//if (Input.GetKey (KeyCode.W)) 
+		//{
+		//	transform.Translate(new Vector3(0,moveSpeed, 0));
+		//}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			transform.Translate(new Vector3(0,-moveSpeed, 0));
+		}
+		//if (temp = temp && !facingRight) 
+		//		Flip ();
+		//else if (temp = temp && facingRight)
+		//		Flip ();
+	
+	}
+
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
+	}
+}
